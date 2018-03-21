0ac0c483d1960435966c8ca9ec4085b559514e3b
gd23 <garrett.j.doty@gmail.com>
2018-03-21 19:43:39 -0400

    Update final.py

--- final.py
+++ final.py
@@ -3,16 +3,23 @@ import tkinter as tk
 from PIL import ImageTk
 import gettingstarted2
 
+root = Tk()
+
 canvas = Canvas(width = 1300, height = 690, bg = 'white')
 canvas.pack(expand = YES, fill = BOTH)
 image = ImageTk.PhotoImage(file = "/Users/Garrett/Desktop/PythonGUI/PughHall.gif")
 canvas.create_image(10,10, image = image, anchor = NW)
 
-def openGraph(self):
+app = Frame(root)
+
+def openGraph(root):
     Graph = gettingstarted2.py
     iconEntry.insert(0, graph)
 
-button1 = Button(fg="green", command = self.openGraph).pack(side=LEFT)
+button1 = Button(app, bg = "green")
+button1.pack()
+
+
 
 
 mainloop()
