4c1019fdee34d6bfcec251eb012439195497ae5c
gd23 <garrett.j.doty@gmail.com>
2018-03-21 20:52:05 -0400

    Update final.py

--- final.py
+++ final.py
@@ -3,23 +3,22 @@ import tkinter as tk
 from PIL import ImageTk
 import gettingstarted2
 
-root = Tk()
+root = tk.Tk()
 
-canvas = Canvas(width = 1300, height = 690, bg = 'white')
+canvas = Canvas(root, width = 1300, height = 690, bg = 'white')
 canvas.pack(expand = YES, fill = BOTH)
 image = ImageTk.PhotoImage(file = "/Users/Garrett/Desktop/PythonGUI/PughHall.gif")
 canvas.create_image(10,10, image = image, anchor = NW)
-
-app = Frame(root)
 
 def openGraph(root):
     Graph = gettingstarted2.py
     iconEntry.insert(0, graph)
 
-button1 = Button(app, bg = "green")
-button1.pack()
+sensor1 = tk.Button(root, text='', bg = "green", command = openGraph, anchor = W)
+sensor1.place(x=40, y=160)
 
-
+sensor2 = tk.Button(root, text='', bg = "green", command = openGraph)
+sensor2.place(x=80, y=250)
 
 
 mainloop()
